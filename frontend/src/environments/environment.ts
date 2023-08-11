dffae5680ebfdb0093a291795a43051b4bf67921
transposter@908 <vasundhara13001@gmail.com>
2023-08-11 13:23:59 +0530

    Changes to gitpod.yml

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'API_URL', // Placeholder for dynamic URL
+  apiUrl: 'https://5000-vsndhra-studentmanageme-5gc5fctr8rk.ws-us102.gitpod.io/', // Placeholder for dynamic URL
 };
