ede845b2c846c9cc3866da0db288e1a66efd0d8b
transposter@908 <vasundhara13001@gmail.com>
2023-08-11 19:36:46 +0530

    CORS issue

--- backend/app.py
+++ backend/app.py
@@ -35,12 +35,12 @@ mysql = MySQL(app)
 # Encrypting the password
 bcrypt = Bcrypt(app)
 
-@app.after_request
-def after_request(response):
-  response.headers.add('Access-Control-Allow-Origin', '*')
-  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-  return response
+# @app.after_request
+# def after_request(response):
+#   response.headers.add('Access-Control-Allow-Origin', '*')
+#   response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+#   response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+#   return response
 
 @app.route('/', methods=['GET'])
 def check():
