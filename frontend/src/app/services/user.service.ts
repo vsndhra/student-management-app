901769dc8ab83f7ca03a3b0b95763726d65c31a7
transposter@908 <vasundhara13001@gmail.com>
2023-08-11 09:47:27 +0530

    modified gitpod yml

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -8,12 +8,12 @@ import { environment } from 'src/environments/environment';
 })
 export class UserService {
 
-  private registerAPI = environment.apiUrl+'/api/register'; // Replace with your Flask API URL for registration
-  private updateAPI = environment.apiUrl+'/api/update'; // Replace with your Flask API URL for updation
-  private addAssignmentAPI = environment.apiUrl+'/api/addAssignment'; // Replace with your Flask API URL for adding assignmen
-  private getAssignmentAPI = environment.apiUrl+'/api/getAssignment'; // Replace with your Flask API URL for getting assignmrnt
-  private submitAssignmentAPI = environment.apiUrl+'/api/submitAssignment'; // Replace with your Flask API URL for getting assignmrnt
-  private getSubmissionAPI = environment.apiUrl+'/api/getSubmission'; // Replace with your Flask API URL for getting assignmrnt
+  private registerAPI = '/api/register'; // Replace with your Flask API URL for registration
+  private updateAPI = '/api/update'; // Replace with your Flask API URL for updation
+  private addAssignmentAPI = '/api/addAssignment'; // Replace with your Flask API URL for adding assignmen
+  private getAssignmentAPI = '/api/getAssignment'; // Replace with your Flask API URL for getting assignmrnt
+  private submitAssignmentAPI = '/api/submitAssignment'; // Replace with your Flask API URL for getting assignmrnt
+  private getSubmissionAPI = '/api/getSubmission'; // Replace with your Flask API URL for getting assignmrnt
 
   constructor(private http: HttpClient) { }
 
